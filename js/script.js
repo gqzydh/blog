0d4feacae2b69596c29e8768571ef2e6df395661
galen <galen@git.xgqq.com>
2018-01-31 10:00:04 +0800

    Site updated: 2018-01-31 10:00:03

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-// 百度统计代码
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?126ea9d39dd27efd454bdba07195b791";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
-
 console.log("开始 测试一下");
