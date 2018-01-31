9f804b400e23553e99bccb9b7844b20e1fc3a549
galen <galen@git.xgqq.com>
2018-01-31 10:04:46 +0800

    Site updated: 2018-01-31 10:04:46

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 console.log("开始 测试一下");
+
+
+// 网盟CNZZ网站统计 /*参考网站：http://www.umeng.com/ */
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1272859133'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1272859133%26online%3D1%26show%3Dline' type='text/javascript'%3E%3C/script%3E"));
