4c80ba87c3da8055843439d3dc3ccc3669253c1a
galen <galen@git.xgqq.com>
2018-12-21 20:34:09 +0800

    Site updated: 2018-12-21 20:34:08

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("开始 测试一下");
+console.log("开始启用");
+
+/*
+* 顶部导航
+* 下拉隐藏
+* 上拉出现
+* */
+
+$(function(){	
+    var cubuk_seviye = $(document).scrollTop();
+    var header_yuksekligi = $('.header-nav').outerHeight();
+
+    $(window).scroll(function() {
+
+        var kaydirma_cubugu = $(document).scrollTop();
+
+        if (kaydirma_cubugu > header_yuksekligi){$('.header-nav').addClass('a-fadeoutT');} 
+        else {$('.header-nav').removeClass('a-fadeoutT');}
+
+        if (kaydirma_cubugu > cubuk_seviye){$('.header-nav').removeClass('a-fadeinT');} 
+        else {$('.header-nav').addClass('a-fadeinT');}				
+
+        cubuk_seviye = $(document).scrollTop();	
+     });
+});
